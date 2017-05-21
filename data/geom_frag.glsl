4787e78a279664cd1bfe763d3e4973d241826b1a
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2017-05-21 20:13:55 +0200

    removed local position

--- data/geom_frag.glsl
+++ data/geom_frag.glsl
@@ -2,7 +2,6 @@
 uniform vec4 color;
 
 in vec4 p;
-in vec4 lp;
 in vec3 n;
 in vec2 t;
 in vec4 c;
@@ -10,12 +9,10 @@ in vec4 c;
 layout(location = 0)out vec3 position;
 layout(location = 1)out vec3 normal;
 layout(location = 2)out vec3 albedo;
-layout(location = 3)out vec3 local_position;
 
 
 void main(void){
     position = vec3(p);
-    local_position = vec3(lp);
     normal = normalize(n);
     albedo = color.xyz * color.a;
 }
