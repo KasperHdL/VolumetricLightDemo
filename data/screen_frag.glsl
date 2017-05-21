84e99b392b2be3206ef9272e2017d8410eca07d9
Kasper Honnens de Lichtenberg <kasper@honnens.dk>
2017-05-21 20:14:27 +0200

    blur

--- data/screen_frag.glsl
+++ data/screen_frag.glsl
@@ -5,6 +5,7 @@ in vec2 uv;
 uniform sampler2D position_texture;
 uniform sampler2D normal_texture;
 uniform sampler2D color_texture;
+uniform sampler2D screen_texture;
 
 uniform vec4 camera_position;
 
@@ -22,20 +23,43 @@ void main(){
     vec3 position       = vec3(texture(position_texture       , uv));
     vec3 normal         = vec3(texture(normal_texture         , uv));
     vec3 albedo         = vec3(texture(color_texture          , uv));
+    vec3 screen         = vec3(texture(screen_texture         , uv));
 
 
+    //dithering
     float a = position_rand;
     position.x += (rand(uv * time * 1)-.5f) * a;
     position.y += (rand(uv * time * 2)-.5f) * a;
     position.z += (rand(uv * time * 3)-.5f) * a;
 
+    //fog
     float l = length(position - vec3(camera_position));
 
     float i = l * fog.w;
     if(i > 1)i = 1;
 
-    color = fog.xyz * i;
+    //blur
+    vec3 blur = vec3(0);
+    int div = 0;
+    const int kernel[9] = int[](
+        1, 1, 1,
+        1, 5, 1,
+        1, 1, 1
+    );
 
+    vec2 texel_size = 1.0  / textureSize(screen_texture, 0);
+    for(int x = -1; x <= 1; x++){
+        for(int y = -1; y <= 1; y++){
+            int k = kernel[(x+1) + (y+1) * 3];
+            blur += texture(screen_texture, uv + vec2(x,y) * texel_size).xyz * k;
+            div += k;
 
+        }
+    }
+    blur /= div;
+    
+
+    //Combine
+    color = blur + fog.xyz * i;
 }
 
